9d3c783c4cc598dbc11ba1547d296eae75f14229
utihirzy <uti831@gmail.com>
2022-01-03 15:00:57 +0700

    fist commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ScoringsComponent } from './scorings/scorings.component';
+import { AdminComponent } from './admin/admin.component';
+import { StandingsComponent } from './standings/standings.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ScoringsComponent,
+    AdminComponent,
+    StandingsComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
